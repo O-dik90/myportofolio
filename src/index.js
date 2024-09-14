c4a239576b1a3db69f3f09a322d58a62026e092d
O-dik90 <odikyudi69@gmail.com>
2024-09-14 21:24:27 +0700

    update hashed router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import App from "App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { MaterialUIControllerProvider } from "context";
 import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
+    <HashRouter>
       <MaterialUIControllerProvider>
         <App />
       </MaterialUIControllerProvider>
-    </BrowserRouter>
+    </HashRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
