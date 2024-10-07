86c5aa0e7b6bbc8373b39e67de362552581f84f0
O-dik90 <odikyudi69@gmail.com>
2024-10-07 18:01:12 +0700

    add looker studio dashboard sampling

--- src/layouts/Projects/index.js
+++ src/layouts/Projects/index.js
@@ -5,7 +5,6 @@ import Footer from "examples/Footer";
 // Material Dashboard 2 React example components
 import Grid from "@mui/material/Grid";
 import MDBox from "components/MDBox";
-import { Typography } from "@mui/material";
 // import data from "layouts/Projects/data/index";
 // import homeDecor1 from "assets/images/home-decor-1.jpg";
 
@@ -34,7 +33,26 @@ function Projects() {
             />
           </Grid>
           {renderData} */}
-          <Typography>Content not Available</Typography>
+          <MDBox
+            sx={{
+              width: "100%",
+              height: 500,
+              border: "2px solid #ccc",
+              borderRadius: 2,
+              overflow: "hidden",
+            }}
+          >
+            <iframe
+              title="looker studio"
+              width="100%"
+              height="675"
+              src="https://lookerstudio.google.com/embed/reporting/37cd31f7-4f5d-48e2-bcf7-95981c2db877/page/A"
+              frameBorder="0"
+              allowFullScreen
+              sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
+            />
+          </MDBox>
+          {/* <Typography>Content not Available</Typography> */}
         </Grid>
       </MDBox>
       <Footer />
